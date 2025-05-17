4646e8ea91180430b3ab007fcf91d06a703a5286
vaishnav <24mp2285@rit.ac.in>
2025-05-17 14:36:27 +0530

    may 17

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,16 @@
-import express from 'express
-';
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/user", (req, res) =>{});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("server started at http://Localhost:5000/user" );
+});
