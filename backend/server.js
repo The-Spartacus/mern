3db101393369eddd4970c51951c9392584cf0d8b
vaishnav <24mp2285@rit.ac.in>
2025-05-27 20:36:46 +0530

    may27

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
-import productRoutes from"./routes/product.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
-app.use("/api/product", productRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
-    console.log("server started at http://Localhost:5000/products" );
+    console.log("server started at http://localhost:" + PORT);
 });
